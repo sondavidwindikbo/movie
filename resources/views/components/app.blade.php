f59bb86ea343b5ae3d1931737fda15cc93ed82db
unknown <windikboson@gmail.com>
2025-12-09 21:40:45 +0900

    Layouting - Membuat Banyak Section Di Satu Component

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -12,9 +12,17 @@
    <x-partials.header></x-partials.header>
     <!-- @include('_partials._header') -->
 
-    <main class="container mx-auto p-5 mt-24">
-        {{ $slot }}
+    <div class="min-h-screen flex">
+        <aside class="w-64 bg-gray-800 p-6 text-white">
+            {{ $sidebar}}
+        </aside>
+
+        <main class="flex-1 p-6 bg-gray-900">
+        {{ $main }}
     </main>
+
+    </div>
+    
 </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,16 @@
 <x-app>
-    <div class="bg-blue-500 text-white py-16 px-8 rounded-lg shadow-lg">
-        <h1 class="text-4xl font-bold">Welocme to Laravel 12</h1>
-        <p class="text-xl mt-6">This is a simple example of Laravel 12.</p>
-    </div>
+    <x-slot name="sidebar">
+        <ul class="space-y-4">
+            <li><a href="#" class="text-white hover:text-gray-300">Dashboard</a></li>
+            <li><a href="#" class="text-white hover:text-gray-300">Profile</a></li>
+            <li><a href="#" class="text-white hover:text-gray-300">Setting</a></li>
+            <li><a href="#" class="text-white hover:text-gray-300">Logout</a></li>
+        </ul>
+    </x-slot>
+    <x-slot name="main">
+        <div class="bg-blue-500 text-white py-16 px-8 rounded-lg shadow-lg">
+            <h1 class="text-4xl font-bold">Welocme to Laravel 12</h1>
+            <p class="text-xl mt-6">This is a simple example of Laravel 12.</p>
+        </div>
+    </x-slot>
 </x-app>
